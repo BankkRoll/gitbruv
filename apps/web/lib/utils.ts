c1d5abd7e0ed368f1b0e97d98ae91cfaf0e81961
Ahmet Kilinc <akx9@icloud.com>
2025-12-23 14:48:22 +0000

    fix

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -17,9 +17,9 @@ export const getPublicServerUrl = () => {
 
 export const getWorkerUrl = () => {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
-    return `https://${process.env.NEXT_PUBLIC_WORKER_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_API_URL}`;
   } else if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
-    return `https://${process.env.NEXT_PUBLIC_WORKER_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_API_URL}`;
   } else {
     return `http://localhost:8787`;
   }
