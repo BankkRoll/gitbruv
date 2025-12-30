f322cd8a723edf1cec8df12eb9ba79dc46ed9a30
Ahmet Kilinc <akx9@icloud.com>
2025-12-30 20:38:02 +0000

    test?

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -8,7 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 export const getPublicServerUrl = () => {
   console.log("PUBLIC SERVER URL", process.env.NODE_ENV, process.env.RAILWAY_PUBLIC_DOMAIN);
   if (process.env.NODE_ENV === "production") {
-    return `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
+    if (process.env.RAILWAY_PUBLIC_DOMAIN) {
+      return `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
+    } else {
+      return `https://${import.meta.env.VITE_RAILWAY_PUBLIC_DOMAIN}`;
+    }
   } else {
     return `http://localhost:3000`;
   }
@@ -17,7 +21,11 @@ export const getPublicServerUrl = () => {
 export const getApiUrl = () => {
   console.log("API URL", process.env.NODE_ENV, process.env.API_URL);
   if (process.env.NODE_ENV === "production") {
-    return `https://${process.env.API_URL}`;
+    if (process.env.API_URL) {
+      return `https://${process.env.API_URL}`;
+    } else {
+      return `https://${import.meta.env.VITE_API_URL}`;
+    }
   } else {
     return `http://localhost:3001`;
   }
